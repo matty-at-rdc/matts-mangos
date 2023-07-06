b523462bef1c99e44c70aa193ebd22731e8e7ef8
Matthew Cale <matt.cale@move.com>
2023-07-06 13:51:55 -0500

    feat: added stylelint and completed about page styling and content

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,7 +28,15 @@ export default () => (
         </p>
       </div>
       <div className='location-info'>
-        <div className='text'>info</div>
+        <div className='text'>
+          <h2>Location Info</h2>
+          <p>As you can imagine, being a fake restaurant finding us can be rather difficult. This will become even more evident when I fail to provide a real address here.</p>
+          <p><span className='location-key'>Address:</span> 2234 Marvelous Mango Mansions, New Dallas, GH, 87653</p>
+          <p><span className='location-key'>Phone:</span> +1 (112) 876-5309</p>
+          <p><span className='location-key'>Email:</span> info@matts-mangos.xyz</p>
+          <p>If that information didn't make it hard enough check out this picture of a non-existent city with no marker for our location whatsoever!</p>
+        </div>
+
         <div className='map'><img className='map' src={img}/></div>
       </div>
 
